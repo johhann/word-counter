1fb5e1f941fe98969d2fa0c27d53feeff7f1099d
Yohannes Mekonnen <johannesm1219@gmail.com>
2021-11-22 21:09:26 +0300

    implement word counter functinality

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express')
+const path = require('path')
+const counter = require('./public/views/counter.html')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use(express.json()); // using user json inputs
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    return res.render(counter);
+});
+
+app.post('/count', (req, res) => {
+    paragraph = req.body.text;
+    paragraph = paragraph.replace(/(^\s*)|(\s*$)/gi,""); // Exclude the start and end spaces
+    paragraph = paragraph.replace(/[ ]{2,}/gi," "); // reduce multiple spaces to a single space
+    paragraph = paragraph.replace(/\n /,"\n"); // exclude a new line with a start spacing
+    counter = paragraph.split(' ').length; // count words
+
+    res.send(`The number of words in your text is: ${counter}`)
+    console.log(`The number of words in your text is: ${counter}`);
+})
+
+// start the server
+app.listen(port, () => console.log(`Server running on port: ${port}`))
